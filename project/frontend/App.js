b2784b2c8a9427fd20822cc0749149054cdfc2e3
Pixie Waffle <sophia_pham@hotmail.com>
2020-09-23 09:26:10 +0200

    merge with nav

--- project/frontend/App.js
+++ project/frontend/App.js
@@ -1,21 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, Button, Alert } from 'react-native';
+import Collect from './components/Collect';
+import Achievements from './components/Achievements';
+import UserSettings from './components/UserSettings';
+import Login from './components/Login';
+
+//import * as React from 'react';
+import { DrawerActions, NavigationContainer } from '@react-navigation/native';
+import 'react-native-gesture-handler';
+import { createAppContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator} from '@react-navigation/drawer';
+
+const RootStack = createStackNavigator()
+
+/*export default function App() {
+  return (
+    <NavigationContainer>{-- Rest of your app code --}</NavigationContainer>
+  );
+}*/
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+      <NavigationContainer>
+        <RootStack.Navigator>
+          <RootStack.Screen name="Collect" component={Collect} />
+          <RootStack.Screen name="UserSettings" component={UserSettings} />
+          <RootStack.Screen name="Login" component={Login} />
+          <RootStack.Screen name="Achievements" component={Achievements} />
+        </RootStack.Navigator>
+    </NavigationContainer>
+    );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    //backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    textAlign: 'center',
+    marginVertical: 8,
   },
 });

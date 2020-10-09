992623aa11ff25ae9d68d49db76ce14df7f53f10
erathiian <42635802+erathiian@users.noreply.github.com>
2020-10-09 10:29:58 +0200

    Delete index.js

--- project/frontend/assets/trashTypesImages/index.js
+++
@@ -1,9 +0,0 @@
-export default {
-  trash: {
-    1: require("./bitbattery.jpg"),
-    2: require("./bitcandywrapper.png"),
-    3: require("./bitcigarette.jpg"),
-    4: require("./bitmetalcan.png"),
-    5: require("./bittrashbag.png"),
-  },
-};

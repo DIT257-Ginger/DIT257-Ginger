667ae2f74faea81655044f9246e7b588f4cc1903
Benjamin Sannholm <benjamin@sannholm.se>
2020-10-13 23:22:25 +0200

    Clean up navigation bar

--- project/frontend/App.js
+++ project/frontend/App.js
@@ -8,11 +8,11 @@ import Home from "./src/screens/Home";
 import { Achievements } from "./src/screens/Achievements";
 import Login from "./src/screens/Login";
 import Analytics from "./src/screens/Analytics";
-import UserSettings from "./src/screens/UserSettings";
 import History from "./src/screens/History";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import Iconn from "react-native-vector-icons/Entypo";
+import IconFA5 from "react-native-vector-icons/FontAwesome5";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import * as Localization from "expo-localization";
 import moment from "moment";
@@ -27,12 +27,12 @@ function MyTabs() {
     <Tab.Navigator
       initialRouteName="Home"
       tabBarOptions={{
-        activeTintColor: "#11402B",
+        activeTintColor: "rgba(0,0,0,0.5)",
         inactiveTintColor: "white",
         style: {
-          paddingTop: "2%",
-          paddingBottom: "5%",
-          height: "12%",
+          paddingTop: 5,
+          paddingBottom: 8,
+          height: 70,
           backgroundColor: "#31A896",
         },
       }}
@@ -41,7 +41,7 @@ function MyTabs() {
         name="Home"
         component={Home}
         options={{
-          tabBarLabel: "Home",
+          tabBarLabel: "Collect",
           tabBarIcon: ({ color, size }) => (
             <Icon name="home" color={color} size={40} />
           ),
@@ -54,13 +54,13 @@ function MyTabs() {
         options={{
           tabBarLabel: "Achievements",
           tabBarIcon: ({ color, size }) => (
-            <Icon name="podium" color={color} size={40} />
+            <IconFA5 name="medal" color={color} size={38} />
           ),
         }}
       />
 
       <Tab.Screen
-        name="line-graph"
+        name="Analytics"
         component={Analytics}
         options={{
           tabBarLabel: "Analytics",
@@ -69,24 +69,14 @@ function MyTabs() {
           ),
         }}
       />
-      <Tab.Screen
-        name="UserSettings"
-        component={UserSettings}
-        options={{
-          tabBarLabel: "Profile",
-          tabBarIcon: ({ color, size }) => (
-            <Icon name="account" color={color} size={40} />
-            //account-cog account-circle
-          ),
-        }}
-      />
+
       <Tab.Screen
         name="History"
         component={History}
         options={{
           tabBarLabel: "History",
           tabBarIcon: ({ color, size }) => (
-            <Icon name="account" color={color} size={40} />
+            <Icon name="history" color={color} size={40} />
           ),
         }}
       />
